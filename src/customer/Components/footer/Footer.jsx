7b95f71559a05107064b544f7a34c1953900b988
lenin12345-dev <leninmohapatra@gmail.com>
2025-03-23 21:12:38 -0400

    dynamic year

--- src/customer/Components/footer/Footer.jsx
+++ src/customer/Components/footer/Footer.jsx
@@ -1,10 +1,14 @@
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
-    <Grid container className='bg-black text-white mt-10 text-center' sx={{ bgcolor: 'black', color: 'white', py: 3 }}>
+    <Grid
+      container
+      className="bg-black text-white mt-10 text-center"
+      sx={{ bgcolor: "black", color: "white", py: 3 }}
+    >
       <Grid item xs={12} sm={6} md={3}>
-        <Typography className='pb-5' variant="h6" gutterBottom>
+        <Typography className="pb-5" variant="h6" gutterBottom>
           Company
         </Typography>
         <Typography variant="body2" component="p" gutterBottom>
@@ -18,7 +22,7 @@ const Footer = () => {
         </Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={3}>
-        <Typography className='pb-5' variant="h6" gutterBottom>
+        <Typography className="pb-5" variant="h6" gutterBottom>
           Solutions
         </Typography>
         <Typography variant="body2" component="p" gutterBottom>
@@ -29,7 +33,7 @@ const Footer = () => {
         </Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={3}>
-        <Typography className='pb-5' variant="h6" gutterBottom>
+        <Typography className="pb-5" variant="h6" gutterBottom>
           Documentation
         </Typography>
         <Typography variant="body2" component="p" gutterBottom>
@@ -37,7 +41,7 @@ const Footer = () => {
         </Typography>
       </Grid>
       <Grid item xs={12} sm={6} md={3}>
-        <Typography className='pb-5' variant="h6" gutterBottom>
+        <Typography className="pb-5" variant="h6" gutterBottom>
           Legal
         </Typography>
         <Typography variant="body2" component="p" gutterBottom>
@@ -47,9 +51,9 @@ const Footer = () => {
           Terms
         </Typography>
       </Grid>
-      <Grid item xs={12} className='pt-10'>
+      <Grid item xs={12} className="pt-10">
         <Typography variant="body2" component="p" align="center">
-          &copy; 2024 My Company. All rights reserved.
+          &copy; {new Date().getFullYear()} My Company. All rights reserved.
         </Typography>
       </Grid>
     </Grid>
