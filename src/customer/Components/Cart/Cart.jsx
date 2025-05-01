f7dca299afd57251ddebb8595c20303c58e09ddf
lenin12345-dev <leninmohapatra@gmail.com>
2025-04-30 22:48:35 -0400

    handle no data

--- src/customer/Components/Cart/Cart.jsx
+++ src/customer/Components/Cart/Cart.jsx
@@ -1,91 +1,100 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CartItem from "./CartItem";
-import { Badge, Button } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { getCart } from "../../../Redux/Customers/Cart/Action";
 import NoDataCard from "../NoDataCard";
-
 
 const Cart = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const jwt = localStorage.getItem("jwt");
-  const {cart,auth}=useSelector(store=>store);
+  const { cart, auth } = useSelector((store) => store);
 
-useEffect(()=>{
-  if (!auth.user){
-    navigate("/login")
-  }
-
-},[auth.user])
   useEffect(() => {
-    if (!auth.user){
-    dispatch(getCart(jwt));
+    if (!auth.user) {
+      navigate("/login");
     }
-  }, [dispatch,jwt,auth.user]);
-  return (
-    <div className="">
-      {cart.cartItems.length>0 && <div className="lg:grid grid-cols-3 lg:px-16 relative">
-        <div className="lg:col-span-2 lg:px-5 bg-white">
-        <div className=" space-y-3">
-          {cart.cartItems.map((item) => (
-            <>
-              <CartItem item={item} showButton={true}/>
-            </>
-          ))}
-              
-        </div>
-      </div>
-      <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0 ">
-        <div className="border p-5 bg-white shadow-lg rounded-md">
-          <p className="font-bold opacity-60 pb-4">PRICE DETAILS</p>
-          <hr />
+  }, [auth.user, navigate]);
 
-          <div className="space-y-3 font-semibold">
-            <div className="flex justify-between pt-3 text-black ">
-              <span>Price ({cart.cart?.totalItem} item)</span>
-              <span>${cart.cart.totalPrice}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Discount</span>
-              <span className="text-green-700">-${cart.cart?.discounte}</span>
-            </div>
-            <div className="flex justify-between">
-              <span>Delivery Charges</span>
-              <span className="text-green-700">Free</span>
-            </div>
-            <hr />
-            <div className="flex justify-between font-bold text-lg">
-              <span>Total Amount</span>
-              <span className="text-green-700">${cart.cart?.totalDiscountedPrice}</span>
+  useEffect(() => {
+    if (auth.user) {
+      dispatch(getCart(jwt));
+    }
+  }, [dispatch, jwt, auth.user]);
+
+  return (
+    <Box
+      className="min-h-[calc(100vh-64px)] flex flex-col"
+      sx={{ backgroundColor: "#f9f9f9" }}
+    >
+      {cart.cartItems.length > 0 ? (
+        <div className="lg:grid grid-cols-3 lg:px-16 relative flex-grow">
+          <div className="lg:col-span-2 lg:px-5 bg-white">
+            <div className="space-y-3">
+              {cart.cartItems.map((item) => (
+                <CartItem key={item._id} item={item} showButton={true} />
+              ))}
             </div>
           </div>
+          <div className="px-5 sticky top-0 h-[100vh] mt-5 lg:mt-0">
+            <div className="border p-5 bg-white shadow-lg rounded-md">
+              <p className="font-bold opacity-60 pb-4">PRICE DETAILS</p>
+              <hr />
 
-          <Button
-            onClick={() => navigate("/checkout?step=2")}
-            variant="contained"
-            type="submit"
-            sx={{ padding: ".8rem 2rem", marginTop: "2rem", width: "100%",
-              backgroundColor: "#f5a623",
-              ":hover": {
-                backgroundColor: "black",
-              },
-             }}
-          >
-            Check Out
-          </Button>
+              <div className="space-y-3 font-semibold">
+                <div className="flex justify-between pt-3 text-black">
+                  <span>Price ({cart.cart?.totalItem} item)</span>
+                  <span>${cart.cart.totalPrice}</span>
+                </div>
+                <div className="flex justify-between">
+                  <span>Discount</span>
+                  <span className="text-green-700">
+                    -${cart.cart?.discounte}
+                  </span>
+                </div>
+                <div className="flex justify-between">
+                  <span>Delivery Charges</span>
+                  <span className="text-green-700">Free</span>
+                </div>
+                <hr />
+                <div className="flex justify-between font-bold text-lg">
+                  <span>Total Amount</span>
+                  <span className="text-green-700">
+                    ${cart.cart?.totalDiscountedPrice}
+                  </span>
+                </div>
+              </div>
+
+              <Button
+                onClick={() => navigate("/checkout?step=2")}
+                variant="contained"
+                type="submit"
+                sx={{
+                  padding: ".8rem 2rem",
+                  marginTop: "2rem",
+                  width: "100%",
+                  backgroundColor: "#f5a623",
+                  ":hover": {
+                    backgroundColor: "black",
+                  },
+                }}
+              >
+                Check Out
+              </Button>
+            </div>
+          </div>
         </div>
-      </div>
-      </div>}
-      {!cart?.cartItems?.length && (
-  <NoDataCard
-    noDataFoundText="No Product Found"
-    styleCardProps={{ style: { height: 600 } }}
-  />
-)}
-    </div>
+      ) : (
+        <Box className="flex-grow flex items-center justify-center">
+          <NoDataCard
+            noDataFoundText="No Product Found"
+            styleCardProps={{  height: "600px",width:"600px",display:"flex",alignItems:"center",justifyContent:"center"} }
+          />
+        </Box>
+      )}
+    </Box>
   );
 };
 
